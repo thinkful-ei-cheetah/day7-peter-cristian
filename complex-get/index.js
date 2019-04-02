17d67df19a04eb3ebb36fed12eeaa7ad2af0e220
Peter Pae <paendabear@gmail.com>
2019-04-02 17:26:48 -0400

    start complex get

--- complex-get/index.js
+++ complex-get/index.js
@@ -1,9 +1,77 @@
-'use strict'
+'use strict';
 /* eslint-disable indent */
 /* eslint-disable strict */
 /* global store cuid $ */
 
 
-const apiKey = "DV3HFODcaoBNRBLk4LBeRW3tZb3FjyVCKElrfUKe";
+const apiKey = 'DV3HFODcaoBNRBLk4LBeRW3tZb3FjyVCKElrfUKe';
 
 
+const searchURL = 'developer.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the items array
+  for (let i = 0; i < responseJson.items.length; i++){
+    // for each video object in the items 
+    //array, add a list item to the results 
+    //list with the video title, description,
+    //and thumbnail
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].snippet.description}</p>
+      <img src='${responseJson.data[i].snippet.thumbnails.default.url}'>
+      </li>`
+    );}
+  //display the results section  
+  $('#results').removeClass('hidden');
+}
+
+function getNationalParks(query, maxResults=10) {
+  const params = {
+    
+    // q: query,
+    stateCode: query,
+    limit: maxResults,
+    start: 1,
+    api_key: apiKey,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+// console.log(url); is showing correct api url.
+
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getNationalParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
